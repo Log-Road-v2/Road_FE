3e4b6fe78936fe68d62bc6b9a47cafcb79f42bda
은지 <wej0513@dsm.hs.kr>
2025-04-09 15:10:41 +0900

    style :: common 버튼 퍼블리싱

---
+++ src/components/Tag/CommonTag.tsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+import { Color, Font } from "../../styles";
+
+const CommonTag = () => {
+  return <Tag>태그</Tag>;
+};
+
+const Tag = styled.div`
+  display: flex;
+  padding: 4px 20px;
+  border: 1px solid ${Color.blue300};
+  border-radius: 20px;
+  background-color: ${Color.white};
+  color: ${Color.blue300};
+  ${Font.medium14}
+`;
+
+export default CommonTag;
