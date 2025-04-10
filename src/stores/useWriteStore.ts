35386e2ba20353c4555f6130a0f38d30694459bc
limda00 <dylim7715@gmail.com>
2025-04-11 08:52:21 +0900

    feat :: 사용자 입력 store 추가

---
+++ src/stores/useWriteStore.ts
@@ -0,0 +1,51 @@
+import { create } from 'zustand'
+
+type AuthorCategory = 'PERSONAL' | 'TEAM';
+
+interface InfoData {
+  contestId: number;
+  projectName: string;
+  authorCategory: AuthorCategory;
+  teamName: string;
+  skills: string[];
+  introduction: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  image: string;
+  vedio: string;
+}
+
+interface WriteStoreType {
+  info: InfoData;
+  setInfo: (data: Partial<InfoData>) => void;
+  reset: () => void;
+}
+
+const initialInfo: InfoData = {
+  contestId: 0,
+  projectName: '',
+  authorCategory: 'PERSONAL',
+  teamName: '',
+  skills: [],
+  introduction: '',
+  description: '',
+  startDate: new Date(),
+  endDate: new Date(),
+  image: '',
+  vedio: ''
+};
+
+export const useWriteStore = create<WriteStoreType>((set) => ({
+  info: initialInfo,
+
+  setInfo: (data) =>
+    set((state) => ({
+      info: {
+        ...state.info,
+        ...data,
+      },
+    })),
+
+  reset: () => set({ info: initialInfo }),
+}));
