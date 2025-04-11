3a948074a57a24977bd4403ac9061c9d1e39c5f2
은지 <wej0513@dsm.hs.kr>
2025-04-11 19:21:49 +0900

    chore :: 라우터 번경

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyles";
 import Layout from "./components/Common/Layout";
 import Main from "./pages/Main";
+import { VotePage } from "./pages/VotePage";
 
 function Router() {
   return (
@@ -9,7 +10,8 @@ function Router() {
       <GlobalStyle />
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/main" element={<Main />}></Route>
+          <Route path="/main" element={<Main />} />
+          <Route path="/Vote" element={<VotePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
