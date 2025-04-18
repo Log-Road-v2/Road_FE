49484e24ecf989c67a100d812d2e3087f0a4684d
은지 <wej0513@dsm.hs.kr>
2025-04-18 09:11:59 +0900

    style :: 회원가입 Role 페이지 퍼블리싱

--- src/components/Common/Button/AuthorButton.tsx
+++ src/components/Common/Button/AuthorButton.tsx
@@ -1,20 +1,26 @@
 import styled from "@emotion/styled";
 import { Color, Font } from "../../../styles";
 
-export const AuthorButton = (isStudent: boolean) => {
+export const AuthorButton = ({ isStudent }: { isStudent: boolean }) => {
   return <Container>{isStudent ? "학생" : "선생님"}</Container>;
 };
 
 const Container = styled.button`
-  width: 100%;
-  max-width: 360px;
-  border: 11px solid ${Color.gray300};
+  width: 360px;
+  height: 44px;
+  border: 1px solid ${Color.gray300};
   color: ${Color.gray300};
   ${Font.semi14}
   background-color: white;
+  border-radius: 8px;
+  padding: 12px 0px;
 
   &:hover {
-    border: 11px solid ${Color.blue300};
+    border: 1px solid ${Color.blue300};
+    color: ${Color.blue500};
+  }
+  &:focus {
+    border: 1px solid ${Color.blue300};
     color: ${Color.blue500};
   }
 `;
--- src/components/Common/Button/SubmitButton.tsx
+++ src/components/Common/Button/SubmitButton.tsx
@@ -1,26 +1,27 @@
 import styled from "@emotion/styled";
-import { Font, Color } from "../../../styles"
+import { Font, Color } from "../../../styles";
 
 interface PropsButtonType {
   text?: string;
-  disabled?: boolean
+  disabled?: boolean;
 }
 
-const ConfirmButton = ({ text, disabled = false }: PropsButtonType) => {
-  return (
-    <Button disabled={disabled}>{text}</Button>
-  )
-}
+const SubmitButton = ({ text, disabled = false }: PropsButtonType) => {
+  return <Button disabled={disabled}>{text}</Button>;
+};
 
 const Button = styled.button<{
-  disabled?: boolean
+  disabled?: boolean;
 }>`
+  width: 360px;
+  height: 44px;
   padding: 12px 56px;
   border-radius: 8px;
   border: none;
-  background-color: ${({ disabled }) => disabled ? Color.gray200 : Color.blue500};
-  color: ${({ disabled }) => disabled ? Color.gray400 : Color.white};
+  background-color: ${({ disabled }) =>
+    disabled ? Color.gray200 : Color.blue500};
+  color: ${({ disabled }) => (disabled ? Color.gray400 : Color.white)};
   ${Font.medium14}
-`
+`;
 
-export default ConfirmButton
+export default SubmitButton;
