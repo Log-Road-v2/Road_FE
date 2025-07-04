59238405400f78191b84630e8595fb521110ea22
limda00 <dylim7715@gmail.com>
2025-07-04 13:21:21 +0900

    add :: 랭킹 페이지 퍼블리싱

--- src/pages/Ranking/style.ts
+++ src/pages/Ranking/style.ts
@@ -76,3 +76,13 @@ export const AwardWrapper = styled.div`
   display: flex;
   gap: 16px;
 `;
+
+export const ModalBackground = styled.div`
+  position: fixed;
+  top: 0; left: 0; right: 0; bottom: 0;
+  background-color: rgba(0,0,0,0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+`;
